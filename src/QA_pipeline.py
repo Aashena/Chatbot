c352f8c135ed291952af102ecb916750e08a73d5
Mostafa Yadegari <mustafa.yadegari@gmail.com>
2026-01-18 23:02:19 -0700

    Fixed the issue of having the UI getting disconnecting during crawling because of some timout issues.

--- src/QA_pipeline.py
+++ src/QA_pipeline.py
@@ -12,7 +12,7 @@ from typing import Optional, Literal
 #Telegram bot communication
 from telegram_handler import create_alert
 
-models_list = ['gemini-2.0-flash-lite', 'gemini-2.5-flash-lite-preview-09-2025', 'gemini-2.0-flash', 'gemini-2.5-flash-preview-09-2025', 'gemini-3-flash-preview', "gemini-2.5-flash-lite" , "gemini-2.5-flash", 'gemini-2.5-pro']
+models_list = ['gemini-3-pro-preview', 'gemini-3-flash-preview' , 'gemini-2.0-flash-lite', 'gemini-2.5-flash-lite-preview-09-2025', 'gemini-2.0-flash', 'gemini-2.5-flash-preview-09-2025', "gemini-2.5-flash-lite" , "gemini-2.5-flash", 'gemini-2.5-pro']
 current_model_idx = 0
 
 PROMPT = ChatPromptTemplate.from_template("""
